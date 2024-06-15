8fb5436bb931f20139adc8073226d8f72ce00655
BydaraujoFilho <bydaraujofilho2021@gmail.com>
2024-06-15 20:46:49 -0300

    Add files via upload

---
+++ prova1 - Copia.py
@@ -0,0 +1,7 @@
+Base = int(input("qual a base do seu Retangulo? "))
+Altura = int(input("qual a Altura do seu retangulo? "))
+resultado = Base*Altura 
+print(f"A area do seu retangulo é {resultado}")
+
+
+#Area = base x Altura
